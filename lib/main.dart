454234cf4138a00dfcc42037b53c079733b5f715
mkizhevsk <mkizhevsk@gmail.com>
2024-05-07 17:15:56 +0400

    tabs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,63 @@
+//import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-//import 'dart:isolate';
-//import 'dart:io';
+//import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(home: MyHome()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Position? _position;
-  //final IsolatedJod ani = new IsolatedJod();
+class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  late int _currentIndex;
 
-  void startGeo() {
-    _getCurrentLocation();
-    asyncTask();
-  }
+  List<Widget> body = const [
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person),
+  ];
 
-  void _getCurrentLocation() async {
-    Position position = await _determinePosition();
-
-    print('position: ' + _position.toString());
-
-    setState(() {
-      _position = position;
-    });
-  }
-
-  Future<Position> _determinePosition() async {
-    LocationPermission permission;
-
-    permission = await Geolocator.checkPermission();
-
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        Future.error('Location Permissions are denied');
-      }
-    }
-
-    //ani.main();
-    //asyncTask();
-
-    return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-  }
-
-  void asyncTask() async {
-    print("start");
-
-    /// Waits for this 5 seconds to elapse
-    while (true) {
-      final result = await Future.delayed(Duration(seconds: 5))
-          .then((result) => _getCurrentLocation());
-    }
-
-    /// Then proceeds to run other lines of code
-    /// While waiting, this does not block off other programs running
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Geolocation App"),
-      ),
       body: Center(
-        child: _position != null
-            ? Text('Current location ' + _position.toString())
-            : Text('No location data'),
+        child: body[_currentIndex],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: startGeo,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Menu',
+            icon: Icon(Icons.menu),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
     );
   }
 }
-
-// class IsolatedJod {
-//   void sayHi(String name) {
-//     while (true) {
-//       sleep(Duration(seconds: 3));
-//       print('Isolate says Hi to $name');
-//     }
-//   }
-
-//   Future<void> main() async {
-//     print('Isolate start');
-//     Isolate isolate = await Isolate.spawn(sayHi, 'mike');
-//   }
-// }
-
-
