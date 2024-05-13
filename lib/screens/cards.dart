6fff0551b660522e40897a84c2cb12a8869d56bf
mkizhevsk <mkizhevsk@gmail.com>
2024-05-13 09:40:33 +0400

    some changes

--- lib/screens/cards.dart
+++ lib/screens/cards.dart
@@ -98,7 +98,7 @@ class CardRow extends StatelessWidget {
         print('onLongPress');
       },
       child: Ink(
-        color: Color.fromARGB(255, 187, 210, 230),
+        color: const Color.fromARGB(255, 187, 210, 230),
         // child: const Center(
         //   child: Text(
         //       'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede'),
